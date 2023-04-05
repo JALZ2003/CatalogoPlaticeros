4ea591f07e24b391aaa21e5a4612e5cfe6ce9eb1
natigalo <natigalo1@gmail.com>
2023-04-04 23:52:19 -0500

    functionCardsCatalogue

--- CatalogosPlaticeros/JS/catalogo.js
+++ CatalogosPlaticeros/JS/catalogo.js
@@ -1,29 +1,61 @@
+const cardsContainer = document.getElementById('cards-container');
 
-const cardsContainer = document.querySelector('.cards-container');
+function createCard(Data) {
 
-for (let index = 0; index < 4; index++) {
+    for (let i = 0; i < Data.length; i++) {
 
-    const productName = "f";
-    const imageUrl = "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=650&amp;w=940%22%3E";
-    const price = 3000;
-    const amount = 52;
-    const category = "f";
-    let card = '<div class="product-card">\n' +
-        '<img src="' + imageUrl + '" alt="' + productName + '" title="' + productName + '" class="product-img">\n' +
+    const nameCard = Data[i].Name;
+    const imgCard = parseInt(Data[i].Img) ;
+    const priceCard = Data[i].Price;
+    const availableCard = Data[i].Available;
+    const category = Data[i].category;
+    let card= cardProduct(nameCard,imgCard,priceCard,availableCard,category)
+    cardsContainer.innerHTML += card;
+    let contenedor= document.querySelector('.product-card')
+    contenedor.addEventListener('click',createAside(nameCard,imgCard,priceCard,availableCard,category, Data[i].Details))
+}
+}
+createCard(DataPlaticeros)
+
+function cardProduct(nameCard,imgCard,priceCard,availableCard,category ) {
+
+    let card = '<div class="product-card">'+
+    '<img src="../assets/images/ imagenesCatalogo/'+ imgCard +'.png">\n' +
         '<div class="product-info">\n' +
         '<div>\n' +
-        '<p>' + price + '</p>\n' +
-        '<p>' + productName + '</p>\n' +
-        '<p>' + "Available: " + amount + '</p>\n' +
+        '<p>' +"$"+ priceCard + '.000'+ '</p>\n' +
+        '<p>' + nameCard + '</p>\n' +
+        '<p>' + "Available: " + availableCard + '</p>\n' +
         '<p>' + "Category: " + category + '</p>\n' +
         '</div>\n' +
         '<figure>\n' +
-        '<img src="./icons/bt_add_to_cart.svg" alt="Add to Cart">\n' +
+        '<img src="../assets/icons/bt_add_to_cart.svg" alt="Add to Cart">\n' +
         '</figure>\n' +
-        '</div>\n' +
+        '</div>\n'+
         '</div>\n';
-
-
-    cardsContainer.innerHTML += card;
+        return card
 }
-
+function createAside(nameCard,imgCard,priceCard,availableCard,category, Details) {
+    const aside = document.getElementById('productDetail')
+    let productDetail = '<aside id="productDetail" class="inactive">\n' +
+    '<div class="product-detail-close">\n' +
+    '<img src="./icons/icon_close.png" alt="close">\n' +
+    '</div>\n' +
+    '<img src="../assets/images/ imagenesCatalogo/'+ imgCard +'.png"\n' +
+    'alt= ' + nameCard + '>\n' +
+    '<div class="product-info">\n' +
+    '<p> "Price: " ' +"$"+ priceCard + '.000'+'</p>\n' +
+    '<p>' + nameCard + '</p>\n' +
+    '<p>' + Details + '</p>\n' +
+    '<p>' + availableCard +'</p>\n' +
+    '<p>' + category +'</p>\n' +
+    
+    '<button class="primary-button add-to-cart-button">\n' +
+    '<img src="./icons/bt_add_to_cart.svg" alt="add to cart">\n' +
+    'Add to cart\n' +
+    '</button>\n' +
+    '</div>\n' +
+    '</aside>';
+    aside.innerHTML+= productDetail
+    aside.style.display=''
+}
