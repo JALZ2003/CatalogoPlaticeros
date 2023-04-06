6c21c09d0206de00b3d0a23f064e02c67cf38165
natigalo <118859761+natigalo@users.noreply.github.com>
2023-04-05 21:32:51 -0500

    added Product cart

--- CatalogosPlaticeros/JS/carShoping.js
+++ CatalogosPlaticeros/JS/carShoping.js
@@ -54,6 +54,43 @@
 //   container.appendChild(shoppingCart);
 // }
 
-
+const container = document.querySelector('.my-order-content');
+const total = document.querySelector('.total');
+let operation = 0;
 let productAddeds = JSON.parse(sessionStorage.getItem('productAdded'));
 console.log(productAddeds);
+
+function creatProductCart(nameCard, imgCard, priceCard) {
+    const shoppingCart = document.createElement('div');
+    shoppingCart.classList.add('shopping-cart');
+
+    const figure = document.createElement('figure');
+    const image = document.createElement('img');
+    image.setAttribute('src', "../assets/images/imagenesCatalogo/" + imgCard + ".png");
+    figure.appendChild(image);
+    shoppingCart.appendChild(figure);
+
+    const name = document.createElement('p');
+    name.textContent = nameCard;
+    shoppingCart.appendChild(name);
+
+    const price = document.createElement('p');
+    price.textContent = '$' + priceCard + '.000';
+    shoppingCart.appendChild(price);
+
+    const closeButton = document.createElement('img');
+    closeButton.setAttribute('src', '../assets/icons/icon_close.png');
+    closeButton.setAttribute('alt', 'close');
+    shoppingCart.appendChild(closeButton);
+    operation += parseInt(priceCard);
+    return shoppingCart;
+}
+
+function insertProductAddeds(Data) {
+    for (let i = 0; i < Data.length; i++) {
+        let product = creatProductCart(Data[i].Name, Data[i].Image, Data[i].Price);
+        container.appendChild(product);
+    }
+    total.textContent = '$' + operation + '.000';
+}
+insertProductAddeds(productAddeds);
