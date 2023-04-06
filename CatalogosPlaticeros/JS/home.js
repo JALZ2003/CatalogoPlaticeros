9793cad67dbd229d60c390c988e25b6f6b923ac2
JALZ <jannus.largo@correounivalle.edu.co>
2023-04-06 18:56:00 -0500

    Added Flags

---
+++ CatalogosPlaticeros/JS/home.js
@@ -0,0 +1,18 @@
+
+// Hacer una solicitud HTTP para obtener información sobre la dirección IP del usuario
+fetch('https://ipapi.co/json/', {
+  mode: 'cors'
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // Acceder a la información de la dirección IP del usuario desde el objeto de respuesta
+    const country = data.country_code.toLowerCase();
+
+    const flagURL = 'https://www.worldometers.info/img/flags/'+country+'-flag.gif';
+
+    const flagimage = document.getElementById('flag-image');
+
+    flagimage.src = flagURL;
+  })
+  .catch(error => console.error(error));
