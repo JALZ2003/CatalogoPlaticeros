e5d84ee2bbc7a6065a98d8697163b33bddaf1dd7
JALZ <jannus.largo@correounivalle.edu.co>
2023-04-05 19:31:52 -0500

    Added Details and Corrections Cards Problems

--- CatalogosPlaticeros/JS/asideCards.js
+++ CatalogosPlaticeros/JS/asideCards.js
@@ -1,23 +1,23 @@
-const aside = document.getElementById('productDetail')
-const imgAside = "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=650&amp;w=940";
-const price = 4;
-const productName = "lorem ";
-const description = " lorem";
-let productDetail = '<aside id="productDetail" class="inactive">\n' +
-    '<div class="product-detail-close">\n' +
-    '<img src="./icons/icon_close.png" alt="close">\n' +
-    '</div>\n' +
-    '<img src="' + imgAside + '"\n' +
-    'alt= ' + productName + '>\n' +
-    '<div class="product-info">\n' +
-    '<p> "Price: " ' + price + '</p>\n' +
-    '<p>' + productName + '</p>\n' +
-    '<p>' + description + '\n' +
-    '</p>\n' +
-    '<button class="primary-button add-to-cart-button">\n' +
-    '<img src="./icons/bt_add_to_cart.svg" alt="add to cart">\n' +
-    'Add to cart\n' +
-    '</button>\n' +
-    '</div>\n' +
-    '</aside>';
-aside.innerHTML += productDetail;
+// const aside = document.getElementById('productDetail')
+// const imgAside = "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=650&amp;w=940";
+// const price = 4;
+// const productName = "lorem ";
+// const description = " lorem";
+// let productDetail = '<aside id="productDetail" class="inactive">\n' +
+//     '<div class="product-detail-close">\n' +
+//     '<img src="./icons/icon_close.png" alt="close">\n' +
+//     '</div>\n' +
+//     '<img src="' + imgAside + '"\n' +
+//     'alt= ' + productName + '>\n' +
+//     '<div class="product-info">\n' +
+//     '<p> "Price: " ' + price + '</p>\n' +
+//     '<p>' + productName + '</p>\n' +
+//     '<p>' + description + '\n' +
+//     '</p>\n' +
+//     '<button class="primary-button add-to-cart-button">\n' +
+//     '<img src="./icons/bt_add_to_cart.svg" alt="add to cart">\n' +
+//     'Add to cart\n' +
+//     '</button>\n' +
+//     '</div>\n' +
+//     '</aside>';
+// aside.innerHTML += productDetail;
--- CatalogosPlaticeros/JS/catalogo.js
+++ CatalogosPlaticeros/JS/catalogo.js
@@ -1,4 +1,6 @@
 const cardsContainer = document.getElementById('cards-container');
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 
 function createCard(Data) {
 
@@ -9,17 +11,16 @@ function createCard(Data) {
         const priceCard = Data[i].Price;
         const availableCard = Data[i].Available;
         const category = Data[i].category;
-        let card = cardProduct(nameCard, imgCard, priceCard, availableCard, category, i);
-        createAside(nameCard, imgCard, priceCard, availableCard, category, Data[i].Details, i);
+        let card = cardProduct(nameCard, imgCard, priceCard, availableCard, category, Data[i].Details);
         cardsContainer.appendChild(card);
     }
 }
 createCard(DataPlaticeros);
 
-function cardProduct(nameCard, imgCard, priceCard, availableCard, category, i) {
+function cardProduct(nameCard, imgCard, priceCard, availableCard, category, details) {
     let content = document.createElement('div');
     content.className = "product-card";
-    let card = '<img src="../assets/images/ imagenesCatalogo/' + imgCard + '.png">\n' +
+    let card = '<img src="../assets/images/imagenesCatalogo/' + imgCard + '.png">\n' +
         '<div class="product-info">\n' +
         '<div>\n' +
         '<p>' + "$" + priceCard + '.000' + '</p>\n' +
@@ -33,51 +34,46 @@ function cardProduct(nameCard, imgCard, priceCard, availableCard, category, i) {
         '</div>\n';
     content.innerHTML += card;
     content.addEventListener('click', () => {
-        sessionStorage.setItem('id', i);
+        sessionStorage.setItem('Name', nameCard);
+        sessionStorage.setItem('Image', imgCard);
+        sessionStorage.setItem('Price', priceCard);
+        sessionStorage.setItem('Avaible', availableCard);
+        sessionStorage.setItem('Category', category);
+        sessionStorage.setItem('details', details);
         viewDestails();
     });
     return content;
 }
 
-function createAside(nameCard, imgCard, priceCard, availableCard, category, Details, i) {
-    const aside = document.getElementById('productDetail')
-    let productDetail = '<aside id="productDetail" class= "inactive  details ' + i + '">\n' +
-        '<div class="product-detail-close">\n' +
-        '<img src="./icons/icon_close.png" alt="close">\n' +
-        '</div>\n' +
-        '<img src="../assets/images/ imagenesCatalogo/' + imgCard + '.png"\n' + 'alt= ' + nameCard + '>\n' +
-        '<div class="product-info">\n' +
-        '<p> "Price: " ' + "$" + priceCard + '.000' + '</p>\n' +
-        '<p>' + nameCard + '</p>\n' +
-        '<p>' + Details + '</p>\n' +
-        '<p>' + availableCard + '</p>\n' +
-        '<p>' + category + '</p>\n' +
-        '<button class="primary-button add-to-cart-button">\n' +
-        '<img src="./icons/bt_add_to_cart.svg" alt="add to cart">\n' +
-        'Add to cart\n' +
-        '</button>\n' +
-        '</div>\n' +
-        '</aside>';
-
-    aside.innerHTML += productDetail;
-}
-
 
 function viewDestails() {
-    let arrayProductDetail = document.querySelectorAll('.details');
-    let containerCards = document.getElementById('main-container');
+    let Name = sessionStorage.getItem('Name');
+    let Image = sessionStorage.getItem('Image');
+    let Price = sessionStorage.getItem('Price');
+    let Avaible = sessionStorage.getItem('Avaible');
+    let Category = sessionStorage.getItem('Category');
+    let details = sessionStorage.getItem('details');
+    let setImage = productDetailContainer.querySelector('.image');
+    setImage.setAttribute('src','../assets/images/imagenesCatalogo/' + Image + '.png');
+    let setPrice = productDetailContainer.querySelector('.price');
+    setPrice.textContent = 'Price: $' + Price + '000';
+    let setName = productDetailContainer.querySelector('.name');
+    setName.textContent = 'Name: ' + Name;
+    let setDetails = productDetailContainer.querySelector('.details');
+    setDetails.textContent = details;
+    let setAvaible = productDetailContainer.querySelector('.avaible');
+    setAvaible.textContent = Avaible;
+    let setCategory = productDetailContainer.querySelector('.category');
+    setCategory.textContent = 'Category: ' + Category;
+    openProductDetailAside();
+}
 
-    arrayProductDetail.forEach((element, index) => {
-        element.className = "inactive details " + index;
-        containerCards.style.width = containerCards.offsetWidth;
-    });
-    let id = sessionStorage.getItem('id');
-    for (let i = 0; i < arrayProductDetail.length; i++) {
-        const element = arrayProductDetail[i];
-        if (element.classList.contains(id)) {
-            element.className = "active details " + id;
-            containerCards.style.width = ((containerCards.offsetWidth - 360) / containerCards.offsetWidth) * 100 + "%";
-            break;
-        }
-    }
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
+
+function openProductDetailAside() {
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside(i) {
+    productDetailContainer.classList.add('inactive');
 }
